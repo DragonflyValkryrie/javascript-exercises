2b0dcc806b22899291a3ba3c1935c9b58774c538
DragonflyValkyrie <148598429+DragonflyValkryrie@users.noreply.github.com>
2023-11-10 21:59:10 -0500

    Completed Assingments 08 to 12

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n < 0) return "OOPS";
 
+    let fibPrev = 0;
+    let fibCurrent = 1;
+  
+    for (let i = 2; i <= n; i++) {
+      let temp = fibCurrent;
+      fibCurrent = fibPrev + fibCurrent;
+      fibPrev = temp;
+    }
+
+    return fibCurrent;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
 
+    const oldest = array.reduce(function(oldest, currentPerson) {
+        const oldestPersonAge = getAge(oldest.yearOfDeath, oldest.yearOfBirth);
+        const currentPersonAge = getAge(currentPerson.yearOfDeath, currentPerson.yearOfBirth);
+        return currentPersonAge > oldestPersonAge ? currentPerson : oldest;
+    });
+    return oldest;
+    
 };
-
+const getAge = function(death, birth){
+    const date = new Date();
+    const currentYear = date.getFullYear();
+    if (death === undefined){
+        return currentYear - birth;
+    } else {
+        return death - birth;
+    }
+        
+    };
 // Do not edit below this line
 module.exports = findTheOldest;
